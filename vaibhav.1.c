477acee34c7074afa50ee6709ebe28fa00e1384d
meabhinav06 <meabhinav2007@gmail.com>
2026-02-12 15:48:27 +0530

    Add files via upload
    sum

---
+++ vaibhav.1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float c,f;
+    clrscr();
+    printf("ënter the temp6rature in celcius=");
+    scanf("%f",&f);
+f=9*c/5+32;
+printf("The temprature in fahrenhite is=%.2f",f);
+}
